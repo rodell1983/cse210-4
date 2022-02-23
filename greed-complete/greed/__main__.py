52373cad7237f26f9c29d251582e012bf2f283f9
jfunky11 <82351460+jfunky11@users.noreply.github.com>
2022-02-22 18:26:13 -0800

    removed artifact from main.py

--- greed-complete/greed/__main__.py
+++ greed-complete/greed/__main__.py
@@ -3,7 +3,6 @@ import random
 import pyray
 
 from game.casting.actor import Actor
-from game.casting.artifact import Artifact
 from game.casting.cast import Cast
 
 from game.directing.director import Director
